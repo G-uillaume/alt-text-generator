e9dbc27451faa98d83102d246459a6c5163ce7c0
NalessoAxel <axelnalesso@gmail.com>
2023-02-04 10:25:10 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+pnpm dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+
+# How it works
+
+Go to [https://replicate.com/] and create a accompte.
+
+Go to your account settings, and create a new API key.
+
+Create a .env file.
+
+copy past your fresh API key into the .env file.
+
+```
+REPLICATE_API_KEY=your_api_key
+```
+
+Copy any image link into the input filed.
+
+Click on generate text button to have your alternative text generated.
+
+This project if forked from [https://vercel.com/templates/next.js/ai-alt-text-generator]
---
+++ pages/index.tsx
@@ -0,0 +1,73 @@
+import type { NextPage } from 'next';
+import { useState } from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
+import Image from 'next/image';
+
+const Home: NextPage = () => {
+	const [imageUrl, setImageUrl] = useState<string>('');
+	const [response, setResponse] = useState<any>(null);
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setImageUrl(e.target.value);
+	};
+
+	const handleClick = async () => {
+		const res = await fetch(`/api/generate?imageUrl=${imageUrl}`);
+		const json = await res.json();
+		console.log(json);
+		setResponse(json);
+	};
+
+	return (
+		<>
+			<Head>
+				<title>Alt Image Generator</title>
+				<meta name="description" content="Alt image generator" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<main className="main">
+				<div className={styles.container}>
+					<h1 className={styles.h1}> Alt Image Generator</h1>
+
+					<input
+						type="text"
+						className={styles.input}
+						value={imageUrl}
+						onChange={(e) => setImageUrl(e.target.value)}
+					/>
+					<Link
+						className={styles.highlight}
+						href="#"
+						onClick={handleClick}
+					>
+						Generate Alt Text
+					</Link>
+				</div>
+
+				<div className={styles.imgContainer}>
+					{imageUrl && (
+						<Image
+							width={700}
+							height={700}
+							style={{
+								objectFit: 'contain',
+							}}
+							src={imageUrl}
+							alt="Image from URL"
+						/>
+					)}
+					{response && (
+						<span className={styles.res}>
+							<p className={styles.p}>{response}</p>
+						</span>
+					)}
+				</div>
+			</main>
+		</>
+	);
+};
+
+export default Home;
